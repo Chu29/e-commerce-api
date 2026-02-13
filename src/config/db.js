4fbbdf88d770467020fbe30f3445171b815cde48
Chu A. <chu.amk22@gmail.com>
2026-02-13 13:19:02 +0100

    feat(db): add PostgreSQL connection setup with Prisma client

---
+++ src/config/db.js
@@ -0,0 +1,47 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import pg from "pg";
+
+const { Pool } = pg;
+
+// Create PostgreSQL connection pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Create Prisma adapter
+const adapter = new PrismaPg(pool);
+
+// Initialize Prisma Client with adapter
+const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "info", "warn", "error"]
+      : ["error"],
+});
+
+// handle db connection
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Database connected Successfully");
+  } catch (error) {
+    console.error(`Database connection failed: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+//handle db disconnect
+const disconnectDB = async () => {
+  try {
+    await prisma.$disconnect();
+    console.log("Database disconnected Successfully");
+  } catch (error) {
+    console.error(`Database disconnection failed: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export { prisma, connectDB, disconnectDB };
