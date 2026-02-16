398496842ad91f2e4984717b5efce51836e84b1d
Chu A. <chu.amk22@gmail.com>
2026-02-16 13:24:56 +0100

    feat(product): implement create product controller for product creation

---
+++ src/modules/product/product.controller.js
@@ -0,0 +1,19 @@
+import { createProduct } from "./product.service.js";
+
+export const createProductController = async (req, res) => {
+  try {
+    const { name, description, price, categoryId, stockQuantity } = req.body;
+    const newProduct = await createProduct({
+      name,
+      description,
+      price,
+      categoryId,
+      stockQuantity,
+    });
+    res
+      .status(201)
+      .json({ message: "Product created successfully", product: newProduct });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
