fbcaae78c3d63cb4b32afa9c380797e5c0eceebb
Chu A. <chu.amk22@gmail.com>
2026-02-16 13:24:39 +0100

    feat(product): implement create product functionality with SKU generation

---
+++ src/modules/product/product.service.js
@@ -0,0 +1,46 @@
+import { prisma } from "../../config/db.js";
+import logger from "../../config/logger.js";
+import { generateUniqueSKU } from "../../utils/skuGenerator.js";
+
+// create new product
+const createProduct = async ({
+  name,
+  description,
+  price,
+  categoryId,
+  stockQuantity,
+}) => {
+  try {
+    const category = await prisma.category.findUnique({
+      where: { id: categoryId },
+    });
+    if (!category) {
+      throw new Error("Category not found");
+    }
+    const sku = await generateUniqueSKU(category.slug);
+    const product = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        categoryId,
+        stockQuantity,
+        sku,
+      },
+    });
+    return product;
+  } catch (error) {
+    logger.error("Error creating product:", error);
+    throw error;
+  }
+};
+
+// get product by id
+
+// get all products
+
+// update product
+
+// delete a product
+
+export { createProduct };
