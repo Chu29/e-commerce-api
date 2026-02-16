57ec5965ef32b47ab04c1c404db57749cb833bed
Chu A. <chu.amk22@gmail.com>
2026-02-16 14:26:21 +0100

    feat(product): add getAllProducts route to retrieve all products

--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { createProductController } from "./product.controller.js";
+import {
+  createProductController,
+  getAllProductsController,
+} from "./product.controller.js";
 
 const router = Router();
 
 // POST - Create new product
 router.post("/products", createProductController);
+
+// GET - Get all products
+router.get("/products", getAllProductsController);
 
 export default router;
