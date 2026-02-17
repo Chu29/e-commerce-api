82bf1e964a0ba5d79021d299f4d8e9124b48e2ed
Chu A. <chu.amk22@gmail.com>
2026-02-17 13:46:02 +0100

    feat(docs): add Swagger documentation for category and product routes

--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -3,6 +3,38 @@ import { createCategoryController } from "./category.controller.js";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Create a new category
+ *     tags: [Categories]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateCategoryRequest'
+ *     responses:
+ *       201:
+ *         description: Category created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Category created successfully"
+ *                 category:
+ *                   $ref: '#/components/schemas/Category'
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 // Create new category
 router.post("/categories", createCategoryController);
 
